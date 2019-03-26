a482b3c6cc140303c6221c4f4963e0c3aeb6dcde
Ryder Lee <ryder.lee@mediatek.com>
2019-03-26 13:16:20 +0800

    mt7615: enlarge rx max pkt len and use full VHT capabilities
    This increases RX throughput.
    
    Signed-off-by: Ryder Lee <ryder.lee@mediatek.com>
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- mt7615/init.c
+++ mt7615/init.c
@@ -41,7 +41,7 @@ static void mt7615_mac_init(struct mt7615_dev *dev)
 	mt7615_mcu_init_mac(dev);
 
 	mt76_wr(dev, MT_DMA_DCR0, MT_DMA_DCR0_RX_VEC_DROP |
-		FIELD_PREP(MT_DMA_DCR0_MAX_RX_LEN, 1024));
+		FIELD_PREP(MT_DMA_DCR0_MAX_RX_LEN, 3072));
 
 	mt76_wr(dev, MT_AGG_ARUCR, FIELD_PREP(MT_AGG_ARxCR_LIMIT(0), 7));
 	mt76_wr(dev, MT_AGG_ARDCR,
@@ -180,6 +180,8 @@ int mt7615_register_device(struct mt7615_dev *dev)
 	dev->mt76.sband_5g.sband.ht_cap.cap |= IEEE80211_HT_CAP_LDPC_CODING;
 	dev->mt76.sband_5g.sband.vht_cap.cap |=
 			IEEE80211_VHT_CAP_SHORT_GI_160 |
+			IEEE80211_VHT_CAP_MAX_MPDU_LENGTH_11454 |
+			IEEE80211_VHT_CAP_MAX_A_MPDU_LENGTH_EXPONENT_MASK |
 			IEEE80211_VHT_CAP_SUPP_CHAN_WIDTH_160_80PLUS80MHZ;
 	dev->mt76.chainmask = 0x404;
 	dev->mt76.antenna_mask = 0xf;
